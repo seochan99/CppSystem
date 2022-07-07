987487f4781199ed2728b3cff5b345ff92f691b4
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-07-07 21:55:49 +0900

    1일차

---
+++ practice_1/practice_1/Chapter1_Problem.cpp
@@ -0,0 +1,73 @@
+//1-1
+//#include <iostream>
+//
+//int main(void)
+//{
+//    int val;
+//    int sum=0;
+//    for(int i=1;i<6;i++){
+//        std::cout<< i << "번째 정수입력 : ";
+//        std::cin>>val;
+//        sum+=val;
+//    }
+//
+//    std::cout<<"합계 : "<<sum<<std::endl;
+//
+//    return 0;
+//}
+
+// 1-2
+//#include <iostream>
+//
+//int main(void)
+//{
+//    char name[100];
+//    char number[200];
+//
+//    std::cout<<"이름은 무엇입니까? ";
+//    std::cin>>name;
+//
+//    std::cout<<"전화번호는? ";
+//    std::cin>>number;
+//
+//    std::cout<<"내 이름은 "<<name<<"입니다. \n";
+//    std::cout<<"제 전화번호는 "<<number<<"입니다."<<std::endl;
+//    return 0;
+//}
+//
+
+// 1-3
+//#include <iostream>
+//
+//int main(void)
+//{
+//    int num;
+//    std::cin>>num; //번호입력
+//    for(int i=1;i<10;i++){
+//        std::cout<<num<<" X "<<i<<" = "<<i*num<<std::endl;
+//    }
+//
+//
+//    return 0;
+//}
+
+// 1-4
+#include <iostream>
+
+int main(void)
+{
+    int num=0;
+    
+    while (1) {
+        std::cout<<"판매 금액을 만원 단위로 입력(-1 to end) : ";
+        std::cin>>num;
+        if(num == -1 )
+        {
+            std::cout<<"프로그램을 종료합니다.\n";
+            break;
+        }
+        std::cout<<"이번달 급여 : "<<num*0.12+50<<"만원\n";
+    }
+    return 0;
+}
+
