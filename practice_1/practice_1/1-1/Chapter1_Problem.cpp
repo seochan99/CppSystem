667bfbb182621999d31337b429e1e1923886c85d
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-07-08 12:27:09 +0900

    1-2

--- practice_1/practice_1/1-1/Chapter1_Problem.cpp
+++ practice_1/practice_1/1-1/Chapter1_Problem.cpp
@@ -52,22 +52,22 @@
 //}
 
 // 1-4
-#include <iostream>
-
-int main(void)
-{
-    int num=0;
-    
-    while (1) {
-        std::cout<<"판매 금액을 만원 단위로 입력(-1 to end) : ";
-        std::cin>>num;
-        if(num == -1 )
-        {
-            std::cout<<"프로그램을 종료합니다.\n";
-            break;
-        }
-        std::cout<<"이번달 급여 : "<<num*0.12+50<<"만원\n";
-    }
-    return 0;
-}
-
+//#include <iostream>
+//
+//int main(void)
+//{
+//    int num=0;
+//
+//    while (1) {
+//        std::cout<<"판매 금액을 만원 단위로 입력(-1 to end) : ";
+//        std::cin>>num;
+//        if(num == -1 )
+//        {
+//            std::cout<<"프로그램을 종료합니다.\n";
+//            break;
+//        }
+//        std::cout<<"이번달 급여 : "<<num*0.12+50<<"만원\n";
+//    }
+//    return 0;
+//}
+//
