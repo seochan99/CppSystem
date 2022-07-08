a2f6e6067c37f3e00e999779a2c73eaca819af6d
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-07-08 15:46:28 +0900

    1-5

---
+++ practice_1/practice_1/1-5/main.cpp
@@ -0,0 +1,10 @@
+//#include "Funcs.hpp"
+//
+//int main(void){
+//    //Best 공간에 simplefunc 함수를 가져온다.
+//    BestComImpl::SimpleFunc();
+//    //prog 공간에 simplefunc 함수를 가져온다.
+//    ProgComImpl::SimpleFunc();
+//    return 0;
+//}
+//
---
+++ practice_1/practice_1/1-5/nameSapce.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+namespace AAA {
+    namespace BBB {
+        namespace CCC {
+            int num1;
+            int num2;
+        }
+    }
+}
+int main(void){
+    AAA::BBB::CCC::num1 = 20;
+    AAA::BBB::CCC::num2 = 30;
+    
+    namespace ABC = AAA::BBB::CCC;
+    cout<<ABC::num1<<endl;
+    cout<<ABC::num2<<endl;
+}
+
+int val = 100; // global
+
+int SimpleFunc(void){
+    int val = 20;
+    val+=3; // 지역변수 증가
+    ::val += 7; //전역변수 증가
+}
+
+
