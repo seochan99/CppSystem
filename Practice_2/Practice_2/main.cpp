5a215548b890a5d1e60f165c707c5d6e48ae4a70
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-07-11 23:00:38 +0900

    2-4

---
+++ Practice_2/Practice_2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//int RefRetFuncOne(int &ref){
+//    ref++;
+//    return ref;
+//}
+
+//int &RefRetFuncOne(int &ref){
+//    ref++;
+//    return ref;
+//}
+//
+//int main(void){
+//    int val1 = 10, val2 = 20;
+//    SwapByRef2(val1, val2);
+//    cout<<"VAL1 : "<<val1<<endl;
+//    cout<<"VAL2 : "<<val2<<endl;
+//    return 0;
+//}
+
+//const int num = 20;
+//int &ref = num;
+//ref+=10;
+//cout<<num<<endl;
+
