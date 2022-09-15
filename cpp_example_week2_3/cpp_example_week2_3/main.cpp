436cfc3842a4e5dc465e252e6c432b3b9c69ddc9
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-15 14:19:39 +0900

    example

--- cpp_example_week2_3/cpp_example_week2_3/main.cpp
+++ cpp_example_week2_3/cpp_example_week2_3/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
@@ -8,14 +7,25 @@ int main(void)
 {
     // 문자열 선언
     string result;
+    int stringLength;
+    
     
     //문자열 입력
+    cout<<"문자열을 입력하세요 : ";
     getline(cin, result);
     
-    // 문자열 뒤집기
-    reverse(result.begin(), result.end());
+    // 문자열 길이 length를 통해 가져오기
+    stringLength = result.length();
     
-    // 문자열 출력
-    cout<<result<<endl;
+ // 문자열  뒤집어서 출력
+    cout<<"반대로 출력 : ";
+    for(int i= stringLength-1;i>=0;i--)
+    {
+        cout<<result[i];
+    }
+    cout<<endl;
+    
+    
+        
     return 0;
 }
