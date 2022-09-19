fa9c793c4271f034c3fb58602e5c421549f4ed62
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-19 11:48:25 +0900

    중간점검-Car클래스만들기

--- cpp_example_week3/cpp_example_week3/main.cpp
+++ cpp_example_week3/cpp_example_week3/main.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
-#include <stdlib.h>
+#include <string>
 
 using namespace std;
 
-class Rectangle{
+// Car 선언부
+class Car{
 public:
-    int width;
-    int height;
-    double getArea();
+    string brand="Chan's Car";
+    int speed = 180;
+    int gear = 4;
+    string color = "Gray";
+    
+    void speedUp();
+    void speedDown();
+    void colorTrans(string changeColor);
 };
 
-// Circle 구현부
-double Rectangle::getArea() {
-    return width*height;
+// Car 구현부
+void Car::speedUp(){
+    speed+=15;
+};
+
+void Car::speedDown(){
+    speed-=10;
+};
+
+void Car::colorTrans(string changeColor){
+    color = changeColor;
 };
 
 int main(void)
 {
-    Rectangle rect;
-    rect.width = 3;
-    rect.height = 5;
-    cout<<"사각형의 면적은 : "<<rect.getArea()<<endl;
+    Car car;
+    cout<<"현재 속도는"<<car.speed<<", 기어는"<<car.gear<<", 현재 자동차 색상은 "<<car.color<<", 현재 자동차 브랜드는 "<<car.brand<<endl;
+    car.speedUp();
+    cout<<"속도 speedUp() 호출 : "<<car.speed<<endl;
+    
+    car.speedDown();
+    cout<<"속도 speedDown() 호출 : "<<car.speed<<endl;
+    
+    car.colorTrans("Black");
+    cout<<"자동차 색상 변경 colorTrans() 호출 : "<<car.color<<"으로 변경"<<endl;
     
     return 0;
 }
