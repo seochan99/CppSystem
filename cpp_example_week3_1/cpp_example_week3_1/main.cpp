58468da36c6189e439a505e0c0d7819abf203300
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-26 13:43:51 +0900

    ㄴㄴ

--- cpp_example_week3_1/cpp_example_week3_1/main.cpp
+++ cpp_example_week3_1/cpp_example_week3_1/main.cpp
@@ -1,29 +1,65 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class CRect{
-    
+class YoutubeSize{
 private:
-    int left,top,right,bottom;
+    int width;
+    int height;
 public:
-    void print(){
-        cout<<"("<<left<<", "<<top<<", "<<right<<", "<<bottom<<")"<<endl;
-    }
-    void SetRect(int l, int t, int r, int b)
-    {
-        left = l;
-        right = r;
-        top = t;
-        bottom = b;
-    }
+    YoutubeSize();
+    YoutubeSize(int, int);
+    int GetWidth();
+    int GetHeight();
+    void ResizeWindow(int,int);
 };
+
+// 이니셜라이저
+YoutubeSize::YoutubeSize() : width(800), height(600){}
+YoutubeSize::YoutubeSize(int w, int h){
+    ResizeWindow(w, h);
+}
+
+int YoutubeSize::GetHeight(){
+    return height;
+}
+
+int YoutubeSize::GetWidth(){
+    return width;
+}
+
+void YoutubeSize::ResizeWindow(int w, int h)
+{
+    if(w>800) width=w;
+    else width = 800;
+    
+    if(h>600) height =h;
+    else height = 600;
+}
 
 int main(void)
 {
-    CRect rc;
-    rc.SetRect(0, 0, 20, 20);
-    rc.print();
+    // 인수 2개를 전달할때의 선언
+    YoutubeSize youtubeSize(1000,800);
+    
+    cout<<"인수 2개 전달받은 youtubeSize width : "<<youtubeSize.GetWidth()<<endl;
+    cout<<"인수 2개 전달받은 youtubeSize height : "<<youtubeSize.GetHeight()<<endl;
+    
+    
+    // resize
+    youtubeSize.ResizeWindow(1400, 600);
+    
+    cout<<"Resize된 인수 2개 전달받은 youtubeSize width :"<<youtubeSize.GetWidth()<<endl;
+    cout<<"Resize된 인수 2개 전달받은 youtubeSize height : "<<youtubeSize.GetHeight()<<endl;
+    
+    // 인수를 전달안할때의 선언
+    YoutubeSize youtubeSize2;
+    
+    cout<<"인수 전달받지 않은 youtubeSize width : "<<youtubeSize2.GetWidth()<<endl;
+    cout<<"인수 전달받지 않은 youtubeSize height : "<<youtubeSize2.GetHeight()<<endl;
+    
+    
     
     return 0;
 }
