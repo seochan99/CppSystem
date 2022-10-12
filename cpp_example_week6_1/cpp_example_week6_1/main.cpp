c4af74ef88dc46d31bdfd02b5bb3294337a332eb
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-10-12 14:02:19 +0900

    6주차 1번

--- cpp_example_week6_1/cpp_example_week6_1/main.cpp
+++ cpp_example_week6_1/cpp_example_week6_1/main.cpp
@@ -3,26 +3,29 @@
 
 using namespace std;
 
+void swap(string lat,int len){
+    cout<<"원본 문자열 : "<<lat<<endl;
+    
+    // 역순 출력
+    cout<<"역순 문자열 : ";
+    // len값을 가지고 와 문자열 뒤부터 출력을 진행함
+    for(int i=len;i>0;i--)
+    {
+        cout<<lat[i];
+    }
+    cout<<endl;
+    
+}
 
 int main(void)
 {
-    string name[5];
+    string latter = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+    int len;
     
-    for(int i=0;i<5;i++){
-        cout<<"NAME : ";
-        getline(cin, name[i], '\n');
-    }
+    len = latter.length();
     
-    string latter = name[0];
-    
-    
-    for(int i=0;i<5;i++){
-        if(name[i].compare(latter)>0)
-            latter = name[i];
-            
-    }
-    cout<<latter<<endl;
-    
+    // 출력함수 실행
+    swap(latter,len);
     
     return 0;
 
