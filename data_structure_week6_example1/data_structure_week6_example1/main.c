c640d6e7dd712df4d2c722b181827fee13046f03
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-10-13 17:01:24 +0900

    6주차 과제

---
+++ data_structure_week6_example1/data_structure_week6_example1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  data_structure_week6_example1
+//
+//  Created by 서희찬 on 2022/10/12.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
