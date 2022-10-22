f17f0b549325a8b4542500bf5ab7bd43859b84e2
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-10-23 03:02:39 +0900

    7주차 3번

--- cpp_example_week7_3/cpp_example_week7_3/main.cpp
+++ cpp_example_week7_3/cpp_example_week7_3/main.cpp
@@ -6,61 +6,44 @@ using namespace std;
 class Factory{
 private:
     char *name; // 저장 공간 확보가 필요한 동적할당 문자 배열
-    int nameLen;
-    
 public:
-    // 모니터 이름을 동적 할당된 문자 배열에 저장
-    Factory(){
-        cout<<"메모리 할당"<<endl;
-    }
-    
-    Factory(char* name){
-        
-        cout<<"메모리 할당"<<endl;
-        
-        // 널문자 삽입을 위한 +1
-        nameLen = (unsigned int)strlen(name)+1;
-        
-        // 동적할당
-        this->name = new char(nameLen);
-        
-        // 문자열 넣기
-        strcpy(this->name, name);
-
-    }
-    
-    // 디폴트 복사 생성자를 사용하였을때 문제 발생
-    Factory(const Factory& f){
-        
-        // 출력해주고 동적할당 해제
-        this->name = f.name;
-        
-        cout<<this->name<<" 1대 생산 !!"<<endl;
-        
-    }
-    
-    ~Factory()
-    {
-        cout<<"메모리 소멸"<<endl;
-        delete []name;
-    }
+    Factory(const char* name); // 생성자
+    Factory(const Factory& f); // 복사생성자
+    ~Factory(); // 소멸자
 };
 
+// 생성자
+Factory::Factory(const char* name){
     
+    int len = (unsigned int)strlen(name); // name길이
+    this->name = new char[len+1]; // \0까지 고려해서 name문자열 공간 할당
+    strcpy(this->name, name); // name에 문자열 복사
+    cout<<"메모리 할당"<<endl;
+}
+
+// 복사생성자
+Factory::Factory(const Factory& f){
+    
+    int len = (unsigned int)strlen(f.name); // name의 문자개수
+    this->name = new char[len+1]; // name을 위한 공간 할당해주기
+    strcpy(this->name, f.name); // this->name에 f.name 문자열 복사
+    cout<<this->name<<" 1대 생산"<<endl;
+}
+
+// 소멸자
+Factory::~Factory(){
+    cout<<"메모리 소멸"<<endl;
+    
+    // name이 동적할당되어있다면
+    if(name)
+        delete []name; // 할당해제 진행
+}
 
 int main(void)
 {
-    // fac1을 동적할당, SAMSUNG이라는 문자가 동적할당 하여 유지되어야지 fac2에서 복사생성가능하기 때문
-    Factory *fac1 = new Factory("SAMSUNG");
-    Factory fac2(*fac1);
-
-    
-    delete fac1;
-    
-    
-    
-    
-
+    // Factory, "SAMSUNG" 주기
+    Factory fac1 = Factory("SAMSUNG");
+    Factory fac2(fac1);
     
     return 0;
 }
