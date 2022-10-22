00a4ddf0204dfa04b00bc5e9f9b005bb3d34cd05
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-10-23 00:50:58 +0900

    7주차 1번문제

--- cpp_example_week7_1/cpp_example_week7_1/main.cpp
+++ cpp_example_week7_1/cpp_example_week7_1/main.cpp
@@ -4,22 +4,23 @@
 using namespace std;
 
 void nojinip(int *t){
+    // 진입 전 속도/3000 km/s로 들어간다.
+    // int형 t를 float형으로 변환해주자.
     cout<<"선박 속도(항구 진입 전) : "<<(float)*t/3000<<"킬로미터(초)"<<endl;
 }
 
-void jinip(int *t)
-{
-    *t = 7000;
-    cout<<"선박 속도(항구 진입 후) : "<<(float)*t/3000<<"킬로미터(초)"<<endl;
+void jinip(int *t){
+    // 진입 후 (기존 속도 - 10000) 나누기 3000 km/s로 간다.
+    // int형 t를 float형으로 변환해주자.
+    cout<<"선박 속도(항구 진입 후) : "<<((float)*t-10000)/3000<<"킬로미터(초)"<<endl;
 }
 
 
 int main(void)
 {
-     
-    int time = 17000;
-    nojinip(&time);
-    jinip(&time);
+    int time = 17000; // 속도
+    nojinip(&time); // 진입전
+    jinip(&time); // 진입후
     return 0;
 }
 
