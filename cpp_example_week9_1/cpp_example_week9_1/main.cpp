d59d226489d92a84afe4e252679464ad40e8d229
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-11-02 13:25:27 +0900

    week9 : constructor overloading

--- cpp_example_week9_1/cpp_example_week9_1/main.cpp
+++ cpp_example_week9_1/cpp_example_week9_1/main.cpp
@@ -2,40 +2,42 @@
 
 using namespace std;
 
-int big(int a,int b){
-    if(a>b)
-        return a;
-    else
-        return b;
+class Person{
+private:
+    int id;
+    double weight;
+    string name;
+public:
+    void show(){
+        cout<<id<<" "<<weight<<" "<<name<<endl;
+    }
+    Person();
+    Person(int id, string name);
+    Person(int id, string name, double weight);
+};
+
+Person::Person(){
+    this->id = 1;
+    this->weight = 10.1;
+    this->name = "chanzzz";
 }
 
-int big(int a[], int size){
-    int res = a[0];
-    for(int i=1;i<size;i++)
-        if(res<a[i])
-            res = a[i];
-    return res;
+Person::Person(int id, string name){
+    this->id = id;
+    this->weight = 20.5;
+    this->name = name;
 }
 
-int small(int a,int b){
-    if(a<b)
-        return a;
-    else
-        return b;
-}
-
-int small(int a[], int size){
-    int res = a[0];
-    for(int i=1;i<size;i++)
-        if(res>a[i])
-            res = a[i];
-    return res;
+Person::Person(int id, string name, double weight){
+    this->id = id;
+    this->weight = weight;
+    this->name = name;
 }
 
 int main(void){
-    int array[] = { 1, 9, -2, 5, 11, 8, 56, 4, 5, 100, 97, 85, 12, 6 };
-    cout << "5와 6중 큰 값 : " << big(5, 6) << endl;
-    cout << "7과 8중 작은 값 : " << small(7, 8) << endl;
-    cout << "최대값 : " << big(array, sizeof(array) / sizeof(array[0])) << endl;
-    cout << "최소값 : " << small(array, sizeof(array) / sizeof(array[0])) << endl;
+    Person seo,chan(2,"chan"),seochan(3,"seochan",60.4);
+    seo.show();
+    chan.show();
+    seochan.show();
+    return 0;
 }
