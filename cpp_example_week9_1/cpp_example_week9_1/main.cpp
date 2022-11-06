bf9cc32470d0047e3ef8186fc2baae98885346ed
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-11-07 03:15:51 +0900

    9주차 실습 - 1

--- cpp_example_week9_1/cpp_example_week9_1/main.cpp
+++ cpp_example_week9_1/cpp_example_week9_1/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath> // sqrt, pow
 #define PI 3.141592
 
 using namespace std;
@@ -10,24 +9,33 @@ private:
     double height; // 높이
     double width; // 너비
     double result;
-    double line;
+    double line; // 선 한길이
 public:
     // 메뉴 출력
     void static menu();
     
-    // 생성자 원, 정사각형
+    // 일반 생성자
+    Measure(){
+    }
+    
+    // 생성자 원
     Measure(double rad){
         this->rad = rad; //반지름주기
     }
     
-    // 생성자 원뿔, 직사각형, 삼각형
-    Measure(double radius=0, double height=0, double width=0){
-        this->radius = radius;
+    // 생성자 직사각형, 삼각형
+    Measure(double height, double width){
         this->height = height;
         this->width = width;
     }
     
+    void SetHeight(double height){
+        this->height = height;
+    }
     
+    void SetLine(double line){
+        this->line = line;
+    }
     
     // 원 면적
     void getCircleArea(){
@@ -43,15 +51,24 @@ public:
     void getTriangleArea(){
         cout<<"삼각형의 면적은 : "<<width*height/2<<endl;
     }
-    
+    // 정사각형
     void getEquilateralRectangleArea(){
-        cout<<"정삼각형 면적은 : "<<rad*rad<<endl;
+        cout<<"정사각형 면적은 : "<<line*line<<endl;
     }
     
+    // 원뿔
     void getOnebbul(){
-        cout<<"원뿔의 부피는 : "<<height
+        cout<<"원뿔의 부피는 : "<<height*(rad*rad*PI)/3<<endl;
     }
     
+    //구의 부피
+    void GetCircleV(){
+        cout<<"구의 부피는 : "<<(rad*rad*rad*PI)*4/3<<endl;;
+    }
+    //실린더 부피
+    void GetSil(){
+        cout<<"실린더의 부피는 : "<< height*(rad*rad*PI)<<endl;
+    }
 };
 
 
@@ -60,6 +77,17 @@ int main(){
     int choice=0;
     double radius;
     double width,height;
+    double line;
+    
+    cout<<"면적 및 부피 계산기"<<endl<<endl;
+    
+    cout<<"1. 원의 면적"<<endl;
+    cout<<"2. 직사각형의 면적"<<endl;
+    cout<<"3. 삼각형 면적"<<endl;
+    cout<<"4. 정사각형 면적"<<endl;
+    cout<<"5. 원뿔의 부피"<<endl;
+    cout<<"6. 구의 부피"<<endl;
+    cout<<"7. 실린더 부피"<<endl<<endl;
     
     // 번호 입력받기
     cout<<"입력 : ";
@@ -73,14 +101,13 @@ int main(){
         case 1: {//원의 면적
             
             // 반지름 입력받기
-            cout<<"반지름 입력 : ";
+            cout<<"원의 반지름 입력 : ";
             cin>>radius;
             cout<<endl;
             
-            // 원 동적생성
+            // 원 동적생성, 반지름 값 1개 필요
             Measure *c = new Measure(radius);
             c->getCircleArea();
-            
             // 할당해제
             delete c;
             break;
@@ -91,7 +118,7 @@ int main(){
             cin>>height>>width;
             cout<<endl;
             
-            // 직사각형 동적생성
+            // 직사각형 동적생성, 높이, 너비 필요
             Measure *r = new Measure(height,width);
             r->getRectangleArea();
             
@@ -106,7 +133,7 @@ int main(){
             cin>>height>>width;
             cout<<endl;
             
-            // 원 동적생성
+            // 원 동적생성, 높이 너비 필요
             Measure *t = new Measure(height,width);
             t->getTriangleArea();
             
@@ -119,40 +146,62 @@ int main(){
         {
             // 정사각형 면적
             cout<<"정사각형 한 변의 길이 입력 : ";
-            cin>>radius;
+            cin>>line;
             cout<<endl;
             
-            // 원 동적생성
-            Measure *et = new Measure(radius);
+            // 정사각형 동적생성
+            Measure *et = new Measure();
+            et->SetLine(line);
             et->getEquilateralRectangleArea();
             // 할당해제
             delete et;
             break;
-    }
+        }
         case 5: //원뿔의 부피 : 밑면의 넓이 * 높이 / 3
-            cout<<"반지름, 높이 입력 : ";
+        {
+            cout<<"원뿔의 반지름, 높이 입력 : ";
             cin>>radius>>height;
             cout<<endl;
             
             // 원 동적생성
-            Measure *c = new Measure(radius,height);
-            et->getEquilateralRectangleArea();
+            Measure *op = new Measure(radius);
+            op->SetHeight(height);
+            
+            op->getOnebbul();
             // 할당해제
-            delete et;
-            break;
-            break;
-            
-        case 6: //구의 부피 : 4/3 * 파이 * 반지름세제곱
-            
-            break;
-        case 7: //실린더 부피 : 밑면의 널빙 * 높이
-            
+            delete op;
             break;
     }
-    
-    // 반지름 입력
-    
-    // 높이 입력
+        case 6:
+        {
+            //구의 부피 : 4/3 * 파이 * 반지름세제곱
+                cout<<"구의 반지름 : ";
+                cin>>radius;
+                cout<<endl;
+                
+                // 원 동적생성
+                Measure *cv = new Measure(radius);
+                cv->GetCircleV();
+                // 할당해제
+                delete cv;
+                break;
+        }
+        case 7: //실린더 부피 : 밑면의 널빙 * 높이
+        {
+            cout<<"실린더의 반지름, 높이 입력 : ";
+            cin>>radius>>height;
+            cout<<endl;
+            
+            // 원 동적생성
+            Measure *sil = new Measure(radius);
+            sil->SetHeight(height);
+            
+            sil->GetSil();
+            // 할당해제
+            delete sil;
+            break;
+        }
+    }
     
     return 0;
 }
