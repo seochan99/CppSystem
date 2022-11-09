d1268ecaa9ba5517ddeb82296b0b9a635f012b0a
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-11-09 14:28:43 +0900

    freinds

--- cpp_example_week10_1/cpp_example_week10_1/main.cpp
+++ cpp_example_week10_1/cpp_example_week10_1/main.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 
 using namespace std;
+class F_test1; // 전방 선언
+class F_test2{
+public:
+    void printInfo(F_test1& F);
+};
+
 
 class F_test1{
     int a;
-    friend class F_test2; // friend keyword
+public:
+    F_test1(int a2=0) :a(a2){}
+    friend void F_test2::printInfo(F_test1& F); // friend keyword
 };
 
-class F_test2{
-public:
-    void set(F_test1& F,int x){ //F_test1 referece
-        F.a =x;
-    }
-    
-    // F_test2는 F_test1 친구이므로 F_test1의 private member에 대한 접근이 가능하다.
-    void printInfo(F_test1& F){
-        cout<<"private - F_test1 a vlaue : "<<F.a<<endl;
-    }
-};
+void F_test2::printInfo(F_test1& F){
+    cout<<"private - F_test1 a vlaue : "<<F.a<<endl;
+}
 
 int main(void)
 {
     F_test1 f1;
     F_test2 f2;
-    
-    f2.set(f1, 777);
-    f2.printInfo(f1); 
+
+    f2.printInfo(f1);
     return 0;
 }
 
