92518d9812292165c1070a7e38bb94c6389a83ef
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-11-09 13:17:47 +0900

    feat : static Member Example

---
+++ cpp_example_week10_1/cpp_example_week10_1/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class Test{
+    int num1;
+    static int a;
+public:
+    Test(){
+        this->num1 = 10;
+    }
+    void printVal(){
+        cout<<"일반 맴버 변수 : "<<num1<<endl;
+        cout<<"static member var : "<<a<<endl;
+        cout<<"------------------------"<<endl;
+    }
+    
+    void setStatic(int x){
+        this->a = x;
+    }
+    void setNum(int x){
+        this->num1 = x;
+    }
+};
+
+
+// static member initialize
+int Test::a = 0;
+
+int main(void)
+{
+    Test t1;
+    Test t2;
+    
+    t1.printVal();
+    t2.printVal();
+    
+    t1.setStatic(100);
+    t2.setNum(22);
+    
+    t1.printVal();
+    t2.printVal();
+    return 0;
+}
+
