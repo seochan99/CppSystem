fc3a425cd9a2a1d583d4d2f3c2302e85d7cb5d2e
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-12-09 18:38:07 +0900

    feat : final exam

---
+++ cpp_example_week14/cpp_example_week14/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_example_week14
+//
+//  Created by 서희찬 on 2022/12/05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
