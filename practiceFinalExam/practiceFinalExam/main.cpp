e2af34b85c9e289795a564d94835cce6e33aefd8
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-12-11 00:54:04 +0900

    final : end

--- practiceFinalExam/practiceFinalExam/main.cpp
+++ practiceFinalExam/practiceFinalExam/main.cpp
@@ -1,23 +1,58 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    char strBuf[100];
-    // (1) get()
-    cin.get(strBuf,100);
+class Histogram{
+    int cnt[26];
+    string str;
+    void makeHistro();
+public:
+    // 생성자 작성
+    Histogram(string str="hi"){
+        this->str = str;
+    }
+    // !연산자 함수 작성
+    void operator!(){
+        // 출력
+        // 히스토그램 만들기
+        cout<<"총 알파벳 수 : "<<str.length()<<endl;
+        makeHistro();
+        for(int i='a';i<='z';i++){
+            cout<<char(i)<<" : ";
+            for(int j=0;j<cnt[i-'a'];j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+    // a = 97
+    // z = 122 
+    // (3) << 연산자 함수 작성
+    template <class T>
+    Histogram& operator << (T s)
+    {
+        str+s;
+        return *this;
+    }
     
-    cout<<strBuf<<endl;
-    // (2)
-    cout<<"get() func : "<<strlen(strBuf)<<"뮨자 읽음 "<<endl<<endl;
-    
-    cin.ignore(); // 입력 버퍼 비우기
+};
 
-    
-    // (4) getline() 사용
-    cin.getline(strBuf, 100);
-    
-    cout<<strBuf<<endl;
-    cout<<"getline() func : "<<strlen(strBuf)<<"뮨자 읽음 "<<endl<<endl;
-    // Enter키까지 읽으므로 get()보다 하나 ccount가 많음
-    
+void Histogram::makeHistro(){
+    for(int i=0;i<26;i++)
+        cnt[i] = 0;
+    int len = int(str.size());
+    for(int i=0;i<len;i++)
+    {
+        if(isalpha(str[i])){
+            char c = tolower(str[i]);
+            cnt[c-'a']++;
+        }
+    }
+}
+
+int main(){
+    Histogram song("People alway call it luck when you've acted more sensibly than tehy have.\n");
+    song<<"My"<<"Universe."<<"-bts"; // 히스토그램에 문자열 추가
+    song<<'y'<<'s'<<'l'; // 히스토그램에 문자 추가
+    !song; //히스토그램 그리기
+    return 0;
 }
